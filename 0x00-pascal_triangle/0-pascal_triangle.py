2b1b564a26cf9e50e3d383baa4727a13b96f54c4
LynneDC <roselinedc1308@gmail.com>
2024-01-08 23:29:06 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
 """
-pascal triangle module
+Pascal triangle module
 """
 
+
 def pascal_triangle(n):
-    """ Create a list of list that reprisent int
-    that represent the pascal triangle 
+    """ Create a list of lists that represent integers
+    representing Pascal's Triangle
     """
     tri = []
-    #check if n is not int or less than 0
+
+    # Check if n is not int or less than 0
     if type(n) is not int or n <= 0:
         return tri
-    #iterate through the list of list
+
+    # Iterate through the list of lists
     for i in range(n):
         a = []
-        #interate the second list of lists
-        for j in range (i + 1):
+
+        # Iterate the second list of lists
+        for j in range(i + 1):
             if j == 0 or j == i:
                 a.append(1)
             elif i > 0 and j > 0:
-                a.append(tri[i -1][j -1] + tri[i -1][j])
+                a.append(tri[i - 1][j - 1] + tri[i - 1][j])
+
         tri.append(a)
-    return tri  
-                        
+
+    return tri
